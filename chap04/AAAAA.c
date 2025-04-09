e8159e41b161297bbabc03027f92be5031dc30ba
mirjam060518@gmail.com <mirjam060518@gmail.com>
2025-04-09 19:38:00 +0900

    2025.04.09-1

--- chap04/AAAAA.c
+++ chap04/AAAAA.c
@@ -1,9 +1,30 @@
+/*
+* 파일명: Core C Programming 2nd Edition Chap04 Programming Assignment00
+* 내용 : 
+* 작성자 : 나영탁
+* 날짜 : 2025.04.00
+* 버전 : v1.0
+*/
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+int assignment0000(void);
 
 int main(void)
 {
-	printf("hi");
-
-	return 0;	
+	assignment0000();
+	return 0;
 }
+
+int assignment0000(void)
+{
+	int a = 0;
+	printf("재생시간(초)? ");
+	scanf("%d", &a);
+
+	printf("%d입니다.", a);
+
+	return 0;
+}
+
