b9e846415e1021bf5272f9cf62c0de451ff46590
mirjam060518@gmail.com <mirjam060518@gmail.com>
2025-05-29 22:57:06 +0900

    2025.05.29

---
+++ chap07/Assignment04.c
@@ -0,0 +1,27 @@
+/*
+* 파일명: Core C Programming 2nd Edition Chap07 Programming Assignment04
+* 내용 : 특정 값으로 초기화된 정수형 배열에 대하여 배열의 원소 중 최댓값을 가진 원소와 최솟값을 가진 원소와 찾아서 인덱스와 값을 출력하는 프로그램을 작성하시오.
+*		 배열의 초기값은 마음대로 정하시오.
+* 작성자 : 나영탁
+* 날짜 : 2025.05.29
+* 버전 : v1.0
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int assignment04(void);
+
+
+int main(void)
+{
+	assignment04();
+
+	return 0;
+}
+
+int assignment04(void)
+{
+
+	return 0;
+}
