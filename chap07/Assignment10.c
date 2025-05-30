a51e860164bb07317f21e8c348ea6058e297a3ca
mirjam060518@gmail.com <mirjam060518@gmail.com>
2025-05-30 18:17:58 +0900

    2025.05.30

---
+++ chap07/Assignment10.c
@@ -0,0 +1,26 @@
+/*
+* 파일명: Core C Programming 2nd Edition Chap07 Programming Assignment10
+* 내용 : 
+* 작성자 : 나영탁
+* 날짜 : 2025.05.30
+* 버전 : v1.0
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int assignment10(void);
+
+
+int main(void)
+{
+	assignment10();
+
+	return 0;
+}
+
+int assignment10(void)
+{
+
+	return 0;
+}
